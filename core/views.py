e11c2f05b2e0c9a10cdca1c435d587f3f46a3079
Marcos <marcosdgomez66@gmail.com>
2025-07-17 15:43:53 -0300

    se arreglo el registro de usuarios

--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from .models import User
 from .forms import RegistroForm
 
 # Create your views here.
@@ -11,7 +11,8 @@ def registroView(request):
     if request.method == 'POST':
         form = RegistroForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('lista')
     else:
         form = RegistroForm()
@@ -28,34 +29,31 @@ def loginView(request):
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
+@login_required
 def logoutView(request):
     logout(request)
     return redirect('login')
 
 @login_required
 def home(request):
-    return HttpResponse('Hola mundo')
+    return HttpResponse('Proyecto Django CRUD')
 
 @login_required
 def listaUsers(request):
     users = User.objects.all()
     return render(request, 'lista.html', {'users':users})
 
-"""def crear_user(request):
-    form = RegistroForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('lista')
-    return render(request, 'formulario.html', {'form':form})"""
-
 @login_required
 def editar_user(request, id):
     user = get_object_or_404(User, id=id)
-    form = RegistroForm(request.POST or None, instance=user)
-    if form.is_valid():
-        form.save()
-        return redirect('lista')
-    return render(request, 'formulario.html', {'form':form})
+    if request.method == 'POST':
+        form = RegistroForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('lista')
+    else:
+        form = RegistroForm(instance=user)
+    return render(request, 'editar.html', {'form': form, 'user': user})
 
 @login_required
 def eliminar_user(request, id):
@@ -63,4 +61,4 @@ def eliminar_user(request, id):
     if request.method == 'POST':
         user.delete()
         return redirect('lista')
-    return render(request, 'eliminar.html', {'user':user})
+    return render(request, 'eliminar.html', {'user': user})
